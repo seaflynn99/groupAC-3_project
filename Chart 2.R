40f321472b854f5a4615956c67738af13dc967f0
Erik Carlson <erikca@uw.edu>
2021-02-19 20:54:49 -0800

    Charts

---
+++ Chart 2.R
@@ -0,0 +1,16 @@
+library(readxl)
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+library("gridExtra")
+
+rookie_salaries <- read_excel("GitHub/groupAC-3_project/data/rookie_salaries.xlsx")
+nba_basketball_reference <- read_excel("GitHub/groupAC-3_project/data/nba_basketball-reference.xlsx")
+wnba_basketball_reference <- read_excel("GitHub/groupAC-3_project/data/wnba_basketball-reference.xlsx")
+
+points_nba <- ggplot(nba_basketball_reference, aes(x=PTS)) + geom_histogram() +
+  ggtitle("NBA Average Points")
+points_wnba <- ggplot(wnba_basketball_reference, aes(x=PTS)) + geom_histogram() +
+  ggtitle("WNBA Average Points")
+histopoints <- grid.arrange(points_nba, points_wnba, nrow = 1) 
+histopoints
